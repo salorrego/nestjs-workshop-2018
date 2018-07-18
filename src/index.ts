c3fbd5cecfa2fbf6284a22d52e3e67154456ae6d
Salomon Orrego Martinez <sorregom@psl.com.co>
2018-07-18 08:59:36 -0500

    adding server with express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, server);
+
+  const healthRouter = express.Router();
+
+  healthRouter.route('/')
+    .get((req, res) => {
+      res.json({ message: 'Application started' });
+    });
+
+  server.use(healthRouter);
+
   await app.listen(3000);
 }
 bootstrap();
