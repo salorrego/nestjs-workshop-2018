7944c68d5996e6d27767c485f53e47a11fa78415
Salomon Orrego Martinez <sorregom@psl.com.co>
2018-08-15 11:01:42 -0500

    adding book controller to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './controllers/app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookService } from './services/book.service';
 import { bookSchema } from './schemas/book.schema';
+import { BookController } from './controllers/book.controller';
 
 @Module({
   controllers: [
-    AppController
+    AppController,
+    BookController
   ],
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/bot', { useNewUrlParser: true }),
