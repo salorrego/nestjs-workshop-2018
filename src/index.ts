54c414f9a6b87c567d79491f6be59e52eb04a323
Salomon Orrego Martinez <sorregom@psl.com.co>
2018-09-25 17:23:41 -0500

    adding mongoose

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as express from 'express';
 
+import * as MongoInMemory from 'mongo-in-memory';
+
+const mongoServerInstance = new MongoInMemory(); // DEFAULT PORT is 27017
+
+mongoServerInstance.start((error, config) => {
+
+  if (error) {
+    console.error(error);
+  } else {
+  // callback when server has started successfully
+
+    console.log('HOST ' + config.host);
+    console.log('PORT ' + config.port);
+
+    const mongouri = mongoServerInstance.getMongouri('test');
+  }
+
+});
+
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, server);
